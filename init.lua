f22b9cc73eb6c7651f913cbd685ff79136e173c4
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-12-16 11:00:01 -0800

    made the variables more user-friendly

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 --[[
 Random voxel generator, made by MushyToast
-set the mx-my values to accomadate your workspace
+set the mx-my values to accomadate your workspace, change the max variable to how many voxels MAX
+Put this script inside ServerScriptService, put a part called 'voxel' in the dimensions that you want the voxel in ServerStorage
+Finally, put a folder called 'voxels' in workspace, this is where all voxels go.
+This is not a ModuleScript, this is a normal ServerScript
 ]]
 local mx = 255
 local mix = -255
@@ -8,11 +11,14 @@ local mz = 255
 local miz = -255
 local miy = 1
 local my = 255
+local maxvoxels = 50000
+
 local folder = game.Workspace:WaitForChild('voxels')
 local voxel = game:GetService('ServerStorage'):WaitForChild('voxel')
 local count = 0
 wait(5)
-while #folder:GetChildren() <= 50000 do
+
+while #folder:GetChildren() <= maxvoxels do
 	count = count + 1
 	local x = math.random(mix, mx)
 	local y = math.random(miy, my)
